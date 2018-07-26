8016b17e53555b575ef9af1f3c76190e88ef71a5
Daniel <danifujii@gmail.com>
2018-07-26 18:23:26 -0300

    Finished Vanilla PG method

--- PolicyGradient.py
+++ PolicyGradient.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from torch.distributions import Categorical
 
 import gym
 import torch
@@ -8,6 +9,7 @@ import torch.nn as nn
 CART_POLE = 'CartPole-v1'
 EPISODES = 1000
 SOLVED_SCORE = 200
+GAMMA = 0.99
 
 
 class Policy(nn.Module):
@@ -16,6 +18,7 @@ class Policy(nn.Module):
         self.fc1 = nn.Linear(input_shape, 32)
         self.fc2 = nn.Linear(32, 32)
         self.output = nn.Linear(32, action_space)
+        self.optimizer = torch.optim.Adam(self.parameters())
 
     def forward(self, input):
         x = functional.relu(self.fc1(input))
@@ -27,10 +30,29 @@ def solved(rewards):
     return len(rewards) > 100 and sum(rewards[-100:]) / 100 >= SOLVED_SCORE
 
 
-def act(policy, state):
+def probs(policy, state):
     state = torch.from_numpy(state).float().unsqueeze(0).cuda()
     probs = policy(state)
-    return torch.argmax(probs).item()
+    dist = Categorical(probs)
+    action = dist.sample()
+    log_prob = dist.log_prob(action)
+    return action.item(), log_prob
+
+
+def update(policy, episode):
+    r = 0
+    discounted_rewards = []
+    policy_loss = []
+    for t in reversed(range(len(episode))):
+        episode_t = episode[t]
+        r = GAMMA * r + episode_t[2]
+        discounted_rewards.append(r)
+        policy_loss.append(-episode_t[3] * r)
+
+    policy.optimizer.zero_grad()
+    policy_loss = torch.cat(policy_loss).sum()
+    policy_loss.backward()
+    policy.optimizer.step()
 
 
 def train(env, policy):
@@ -44,14 +66,13 @@ def train(env, policy):
         episode = []
 
         while not done:
-            action = act(policy, state)
-            _, reward, done, _ = env.step(action)
-            episode.append((state, action, reward))
-
+            action, logp = probs(policy, state)
+            state, reward, done, _ = env.step(action)
+            episode.append((state, action, reward, logp))
             game_reward += reward
 
         print("Reward: " + str(game_reward) + " at " + str(datetime.now()))
-
+        update(policy, episode)
         rewards.append(game_reward)
         if solved(rewards):
             break
